e3ec4c79bc08971f01e8c8a6206bcf9c0834672d
Houssien Zeineddine <houssienzd@hotmail.com>
2025-03-01 01:49:37 +0200

    integrating frontend-backend for createUser api

--- wallet_client/JS/main.js
+++ wallet_client/JS/main.js
@@ -19,3 +19,29 @@ const closeNav = () => {
 
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+
+document.getElementById("signupForm").addEventListener("submit", async (event) => {
+    event.preventDefault(); // Prevent the form from submitting the default way
+
+    const formData = new FormData(event.target);
+
+    axios.post("/wallet_server/signup.php", formData , {
+        headers: {
+            "Content-type" : "multipart/form-data"
+        }
+    })
+    .then((response) => {
+        const result = response.data;
+
+        if(result.status === "success") {
+            document.querySelector(".alert_message.success").style.display = "block"; //i may delete this line later
+            document.querySelector(".alert_message.error").style.display = "none";
+            document.querySelector(".success_message").textContent = result.message;
+        } else {
+            document.querySelector(".alert_message.success").style.display = "none"; //i may delete this line later
+            document.querySelector(".alert_message.error").style.display = "block";
+            document.querySelector(".error_message").textContent = result.message;
+        }
+    })
+})
