5e166a2571f164ea5f1ef86d1c795684cef3f6b2
Houssien Zeineddine <houssienzd@hotmail.com>
2025-03-02 14:25:22 +0200

    adding the dashboard php token check but if course it has bug that need to be fixed

--- wallet_server/apis/dashboard.php
+++ wallet_server/apis/dashboard.php
@@ -3,4 +3,16 @@
     session_start();
     include('../utils/jwt.php');
 
+    if(!isset($_SESSION['token'])) {
+        header('Location: ../wallet_client/sgingin.html');
+        exit();
+    }
+
+    $decoded = verifyToken($_SESSION['token']);
+
+    if(!$decoded) {
+        header('Location: ../wallet_client/sgingin.html');
+        exit();
+    }
+
 ?>
